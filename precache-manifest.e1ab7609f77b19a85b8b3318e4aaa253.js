6d7bbf2f757654d2ec7a030dea53198ef8fe9653
João Gardenberg <joaogardenberg@gmail.com>
2020-07-07 13:12:08 -0300

    Updates

---
+++ precache-manifest.e1ab7609f77b19a85b8b3318e4aaa253.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25884d3ee5408e1338b18f2b7df0ead1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9008282d63e3a97a897b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d7036f0ed27038423616",
+    "url": "/static/js/2.4d7cf4ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4d7cf4ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9008282d63e3a97a897b",
+    "url": "/static/js/main.4a0630a2.chunk.js"
+  },
+  {
+    "revision": "f7280298f96ff265857b",
+    "url": "/static/js/runtime-main.a5ddd411.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
